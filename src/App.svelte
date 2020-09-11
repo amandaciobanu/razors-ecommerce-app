c807d0ea95ef538fbd37390585766f1ff18a2bc2
Amanda Ciobanu <amanda@ciobanu.org>
2020-09-11 13:33:32 +0100

    chore: setup folders and pages

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+	//pages
+  import Login from './pages/Login.svelte';
+  import About from './pages/About.svelte';
+  import Checkout from './pages/Checkout.svelte';
+  import Home from './pages/Home.svelte';
+  import Products from './pages/Products.svelte';
+  import ProductTemplate from './pages/ProductTemplate.svelte';
+
+
+
+</script>
+
+
+	<h1>Hello !</h1>
+	
