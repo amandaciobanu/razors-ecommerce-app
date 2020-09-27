4196aaf7f9948decc95a52a9c84922c06ab76d8a
Amanda Ciobanu <amanda@ciobanu.org>
2020-09-27 09:28:59 +0100

    feat: rendering products index page

--- src/components/Products/Products.svelte
+++ src/components/Products/Products.svelte
@@ -1,20 +1,16 @@
 <script>
+  export let title = '';
   import  {onMount, onDestroy} from 'svelte'
   import products from '../../stores/defaultProducts'
-  let localProducts =[];
-  /*let unsubscribe;
-/!*<!--  onMount(() => {-->
-<!--  unsubscribe = products.subscribe( value =>{-->
-<!--    localProducts = value-->
-<!--  });-->
-<!--});-->
-<!--  -->
-<!--  onDestroy(() => {-->
-<!--    unsubscribe();-->
-//   })*!/*/
+  import Product from './Product.svelte'
 </script>
 
-<h1>hello from prod components</h1>
-{#each localProducts as product}
-  <h2>{product.title}</h2>
-  {/each}
+<section class="section">
+  <h2 class="section-title">{title}</h2>
+  <div class="products-center">
+    {#each $products  as product(product.id)}
+      <Product {product}/>
+    {/each}
+  </div>
+  
+</section>
