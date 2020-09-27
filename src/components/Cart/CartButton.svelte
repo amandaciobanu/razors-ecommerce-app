432fac599d9786a945f7d249253a4d753f8e649b
Amanda Ciobanu <amanda@ciobanu.org>
2020-09-27 12:18:13 +0100

    feat: small navbar

---
+++ src/components/Cart/CartButton.svelte
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+<!--cart toggle-->
+<div class="btn-cart-container">
+  <button class="btn-cart-toggle" on:click="{() =>
+    console.log('cart toggle')
+  }">
+    <i class="fas fa-cart-plus">
+    
+    </i>
+  </button>
+  <span class="btn-cart-items"></span>
+</div>
